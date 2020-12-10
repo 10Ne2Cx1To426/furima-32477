4752f5b79b16b60e6f28f019672e6e2a193867cf
Sena <NseCnaT@users.noreply.github.com>
2020-12-11 00:06:26 +0900

    itemモデルの単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,75 @@ RSpec.describe Item, type: :model do
     before do
       @item = FactoryBot.build(:item)
     end
-
-    
+    it 'name, text, image, category_id, condition_id, delivery_fee_id, prefecture_id, delivery_day_id, priceが存在すれば保存できる ' do
+      expect(@item).to be_valid
+    end
+    it 'nameが空だと保存できない' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'textが空だと保存できない' do
+      @item.text = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Text can't be blank")
+    end
+    it 'imageが空だと保存できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'priceが空だと保存できない' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it 'priceが半角数字以外だと保存できない' do
+      @item.price = "１"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it 'priceが300より下だと保存できない' do
+      @item.price = 200
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it 'priceが9,999,999より大きいと保存できない' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+    it 'categoryが---(id:1)だと保存できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    it 'conditionが---(id:1)だと保存できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+    it 'delivery_dayが---(id:1)だと保存できない' do
+      @item.delivery_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
+    end
+    it 'delivery_feeが---(id:1)だと保存できない' do
+      @item.delivery_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+    end
+    it 'prefectureが---(id:1)だと保存できない' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+    it 'ユーザーが紐づいていないと保存できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
   end
 end
+
+# bundle exec rspec spec/models/item_spec.rb
