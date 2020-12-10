a276902db49b573c03ef36274cdacb51b1493c54
Sena <NseCnaT@users.noreply.github.com>
2020-12-10 23:26:53 +0900

    単体モデルの準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -8,7 +9,11 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   
-  validates :name, :text, :price, presence: true
+  validates :name, :text, :image, presence: true
+
+  with_options presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
+    validates :price
+  end
 
   validates :category_id, :condition_id, :delivery_day_id, :delivery_fee_id, :prefecture_id, numericality: { other_than: 1 } 
 end
