dbf326188b7940938b05d26684f441ae0c6edc20
Sena <NseCnaT@users.noreply.github.com>
2020-12-11 11:38:11 +0900

    値の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
     it 'priceが300より下だと保存できない' do
-      @item.price = 200
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
     end
